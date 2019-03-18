f0b032f24380279d74e7d3e572be1d23552a6f64
Luigi <thegaminggodlou@gmail.com>
2019-03-18 14:22:35 -0700

    Updates

---
+++ precache-manifest.254b968efce7623de3741c0fbb6feb72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4b2761a3caf8a530534",
+    "url": "/carousel-live/static/js/runtime~main.6a3a7d66.js"
+  },
+  {
+    "revision": "4673601fe7f31afe83c8",
+    "url": "/carousel-live/static/js/main.a91247a3.chunk.js"
+  },
+  {
+    "revision": "8b99271e080eb188b284",
+    "url": "/carousel-live/static/js/2.3c727297.chunk.js"
+  },
+  {
+    "revision": "4673601fe7f31afe83c8",
+    "url": "/carousel-live/static/css/main.bb73dd97.chunk.css"
+  },
+  {
+    "revision": "95892f6d80264ec73c408b71c165934d",
+    "url": "/carousel-live/index.html"
+  }
+];
